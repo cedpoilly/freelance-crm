4f4256c0d3373146a69d34f30f4df798763c4bda
Cedric Poilly <cedric.poilly@theredpandas.com>
2022-07-22 11:34:55 +0400

    Homemade datatable, with row click handler

--- front/src/components/DataTable.vue
+++ front/src/components/DataTable.vue
@@ -1,13 +1,5 @@
-<!-- 
-  Currently using the follwing third party, see documentation:
-  https://gridjs.io/
-  -->
-
 <script setup>
-import { onMounted, ref, toRaw, watch } from "vue"
-
-import { Grid } from "gridjs"
-import "gridjs/dist/theme/mermaid.css"
+import { onMounted, ref, watch } from "vue"
 
 const props = defineProps({
   data: {
@@ -16,68 +8,116 @@ const props = defineProps({
   }
 })
 
-const gridRef = ref(null)
+const { dataHeaders, headers } = getHeadersFromList(props.data)
+const data = ref([])
 
-onMounted(() => {
-  const headers = getHeadersFromList(props.data)
-  gridRef.value = new Grid({ columns: headers, data: props.data })
-    .render(document.getElementById("wrapper"))
-})
+onMounted(() => setData(props.data, dataHeaders))
 
-watch(() => props.data, (newValue) => {
-  gridRef.value
-    .updateConfig({ data: newValue })
-    .forceRender()
-})
+watch(() => props.data, (newValue) => setData(newValue, dataHeaders))
+
+function setData(list) {
+  data.value = getRowsFromList(list, dataHeaders)
+}
 
 function getHeadersFromList(list) {
   const firstItem = list[0]
-  const headers = getHeadersFromObject(
+  const { dataHeaders, headers } = getHeadersFromObject(
     firstItem,
-    ["_id", "projects", "whatsAppNumber", "isCodementor"]
+    ["_id", "projects", "whatsAppNumber", "isCodementor", "tags"]
   )
-  return headers
+  return { dataHeaders, headers }
 }
 
 function getHeadersFromObject(object, filterList) {
-  return Object
+  const dataHeaders = Object
     .keys(object)
     .filter(item => !filterList.includes(item))
     .map(raw => {
       switch (raw) {
         case "firstName": return {
-          id: "firstName", name: "First Name", width: "10%"
+          id: "firstName", name: "First Name", width: "w-48"
         }
         case "lastName": return {
-          id: "lastName", name: "Last Name", width: "10%"
+          id: "lastName", name: "Last Name", width: "w-48"
         }
         case "email": return {
-          id: "email", name: "Email", width: "10%"
+          id: "email", name: "Email", width: "w-96"
         }
         case "level": return {
-          id: "level", name: "Level", width: "10%"
+          id: "level", name: "Level", width: "w-44"
         }
         case "serviceType": return {
-          id: "serviceType", name: "Service", width: "10%"
+          id: "serviceType", name: "Service", width: "w-40"
         }
         case "rate": return {
-          id: "rate", name: "Rate (USD)", width: "10%"
+          id: "rate", name: "Rate (USD)", width: "w-36"
         }
         case "tags": return {
           id: "tags",
           name: "Tags",
-          width: "31%",
+          width: "w-48",
           formatter: (cell, row, column) => {
             return cell.join(" | ")
           }
         }
+
         default: return raw
       }
     })
+
+  const actionHeader = {
+    id: 'actions',
+    name: "Actions",
+    width: "w-24",
+  }
+
+  const headers = [...dataHeaders, actionHeader]
+
+  return { dataHeaders, headers }
 }
 
+function getRowsFromList(list, headers) {
+  return list.map(item => headers.map(header => item[header.id]))
+}
+
+function openModal() {
+  alert("TODO: Open modal! 😃")
+}
 </script>
 
 <template>
-  <div id="wrapper" class="w-3/4 mx-auto mt-6 pt-6"></div>
+  <div id="datatable" class="w-3/4 mx-auto mt-6 pt-6 overflow-x-hidden">
+    <div class="header w-full flex">
+      <span :class="header.width" class="header-cell border bg-gray-100 rounded" v-for="(header, index) in headers"
+        :key="index">{{
+            header.name
+        }}</span>
+    </div>
+
+    <div class="body w-full flex flex-col contents rounded">
+      <div class="w-full flex hover:scale-101 transform-gpu ease-in	duration-75" title="Click to view all details."
+        @click="openModal" v-for="(row, index) in data" :key="index">
+        <span class="data-cell border bg-gray-100" :class="headers[cellIndex].width" v-for="(cell, cellIndex) in row">{{
+            cell
+        }}
+        </span>
+
+        <span class="data-cell border bg-gray-100" :class="headers[row.length].width">{{ headers[row.length].width
+        }}
+        </span>
+      </div>
+    </div>
+    <div class="footer"></div>
+  </div>
 </template>
+
+<style lang="scss">
+.header-cell,
+.data-cell {
+  @apply px-3 py-2
+}
+
+.header-cell {
+  @apply font-bold
+}
+</style>
--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -5,7 +5,11 @@ module.exports = {
     "./src/**/*.{vue,js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      scale: {
+        '101': '1.005',
+      }
+    }
   },
   plugins: [],
 }
