36fa5aea8e5dd9325515b7281b00af02be52ca9e
Cedric Poilly <cedric.poilly@theredpandas.com>
2022-07-22 17:26:28 +0400

    Update base toggle

--- front/src/components/BaseToggle.vue
+++ front/src/components/BaseToggle.vue
@@ -15,16 +15,20 @@ function emitToggle() {
 <template>
   <label :for="props.fieldName" class="base-toggle" @click="emitToggle">
     <span class="base-toggle-label">{{ props.label }}</span>
-    <input type="checkbox" :id="props.label" :name="props.label" :checked="props.isChecked">
+    <input class="base-toggle-cb" type="checkbox" :id="props.label" :name="props.label" :checked="props.isChecked">
   </label>
 </template>
 
 <style lang="scss" scoped>
 .base-toggle {
-  @apply w-80 h-16 mx-2 px-4 py-3 mb-3 border rounded flex justify-between bg-white
+  @apply w-80 h-16 mx-2 px-4 py-3 mb-3 border rounded flex justify-between items-center bg-white
 }
 
 .base-toggle-label {
   @apply flex items-center
+}
+
+.base-toggle-cb {
+  @apply block w-5 h-5 px-3
 }
 </style>
