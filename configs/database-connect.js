04c4d60354fad2800895978f705bd57ddc702452
Cedric Poilly <cedric.poilly@theredpandas.com>
2022-07-23 20:06:41 +0400

    Connect to Atlas if HEROKU

--- configs/database-connect.js
+++ configs/database-connect.js
@@ -1,11 +1,11 @@
 const { DB_HOST, DB_NAME, DB_USER_PASSWORD } = require("./database")
 const mongoose = require('mongoose')
 
-const connectionString = process.env.NODE_ENV === "prod"
+const connectionString = process.env.IS_HEROKU === "true"
   ? `mongodb+srv://dbuser:${DB_USER_PASSWORD}@cluster0.fmcc5.mongodb.net/?retryWrites=true&w=majority`
   : `mongodb://${DB_HOST}/${DB_NAME}`
 
-mongoose.connect(`mongodb+srv://dbuser:${DB_USER_PASSWORD}@cluster0.fmcc5.mongodb.net/?retryWrites=true&w=majority`, { useNewUrlParser: true })
+mongoose.connect(connectionString, { useNewUrlParser: true })
 
 mongoose.connection
   .once('open', function connected() { console.log("Connected [Mongoose - MongoDB]") })
