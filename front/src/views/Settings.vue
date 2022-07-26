08d7309b6bf985293bca42caaa46954b2889f0d1
Cedric Poilly <cedric.poilly@theredpandas.com>
2022-07-26 14:29:57 +0400

    Styling

--- front/src/views/Settings.vue
+++ front/src/views/Settings.vue
@@ -1,6 +1,8 @@
 <template>
-  <div class="view-container w-full mt-10">
-    <h2 class="w-full text-center text-2xl">Welcome to the settings page.</h2>
-    <h3 class="w-full text-center text-xl">Options are coming soon. 🚧</h3>
+  <div class="view-container w-full">
+    <div class="w-full pt-10">
+      <h2 class="w-full text-center text-2xl">Welcome to the settings page.</h2>
+      <h3 class="w-full text-center text-xl">Options are coming soon. 🚧</h3>
+    </div>
   </div>
 </template>
