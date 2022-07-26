8833049d27dfdd101150c2964275acd8250f1b62
Cedric Poilly <cedric.poilly@theredpandas.com>
2022-07-26 18:36:18 +0400

    Formatting

--- front/tailwind.config.cjs
+++ front/tailwind.config.cjs
@@ -1,22 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    "./index.html",
-    "./src/**/*.{vue,js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
   theme: {
     extend: {
       scale: {
-        '101': '1.005',
+        101: "1.005",
       },
 
       colors: {
-        blue: '#1A91FF'
-      }
-    }
+        blue: "#1A91FF",
+      },
+    },
   },
 
-  darkMode: 'class',
+  darkMode: "class",
 
   plugins: [],
 }
