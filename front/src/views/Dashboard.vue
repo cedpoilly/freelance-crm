847eb065d865264fd85ec70a86920e17ea73f22f
Cedric Poilly <cedric.poilly@theredpandas.com>
2022-07-26 12:18:10 +0400

    Dashboard refactor

--- front/src/views/Dashboard.vue
+++ front/src/views/Dashboard.vue
@@ -1,34 +1,32 @@
-<script setup>
+<script>
   import { onBeforeMount, onMounted, ref } from "vue"
-
-  import Client from "../models/Client"
 
   import Toolbar from "../components/Toolbar.vue"
   import DataTable from "../components/DataTable.vue"
   import ClientModal from "../components/ClientModal.vue"
 
-  import useAlert from "../app/alert"
-
   import useHelpers from "../app/helpers"
+  import useDasboard from "./dashboard"
 
-  import { getClients, createClient, updateClient } from "../api/client"
+  const { ctrlPlus } = useHelpers()
 
-  const { alert } = useAlert()
+  const { data, selectedClient, fetchTableData, filter, filterData } = useDasboard()
 
-  const { searchStringInList, getCopy, ctrlPlus } = useHelpers()
+</script>
 
+<script setup>
+// * must stay in `<script setup>`
   const emits = defineEmits(["focus-navbar", "notification"])
-
-  let initialData = []
-  const data = ref([])
-  const selectedClient = ref({})
 
   onBeforeMount(() => fetchTableData())
 
   // * template refs
-  const modal = ref(null)
-  const toolbar = ref(null)
-  const dataTable = ref(null)
+  const { modal, toolbar, dataTable } = useDasboard()
+
+  // * template refs direct methods
+  const { focusDataTable, focusSearch } = useDasboard()
+
+  const { openModal, openCreateModal } = useDasboard({ notify })
 
   onMounted(() => {
     document.onkeydown = async event => {
@@ -40,57 +38,6 @@
       ctrlPlus(event, ">", openCreateModal)
     }
   })
-
-  async function fetchTableData() {
-    const response = await getClients()
-    const dataFromServer = await response.json()
-    data.value = [...dataFromServer]
-    initialData = [...dataFromServer]
-  }
-
-  function filter(field, value) {
-    switch (field) {
-      case "is-from-codementor": {
-        if (!value) {
-          return (data.value = initialData)
-        }
-
-        data.value = initialData.filter(item => item.isCodementor === value)
-        break
-      }
-
-      case "tags": {
-        if (!value || !value.length) {
-          return (data.value = initialData)
-        }
-
-        const filteredData = initialData.filter(item =>
-          value.every(tag => !!item.tags.includes(tag))
-        )
-
-        data.value = filteredData || initialData
-        break
-      }
-
-      default: {
-        data.value = initialData
-        break
-      }
-    }
-  }
-
-  function filterData(searchString) {
-    const noSearchString = !searchString
-    if (noSearchString) {
-      data.value = initialData
-      return
-    }
-
-    const filteredList = searchStringInList(initialData, searchString, {
-      isObejectList: true,
-    })
-    data.value = [...filteredList]
-  }
 
   async function saveOnCtrlS(event) {
     if (event.ctrlKey && event.key === "s") {
@@ -106,140 +53,12 @@
     }
   }
 
-  function focusSearch() {
-    toolbar.value.focusSearch()
-  }
-
   function focusNavbar() {
     emits("focus-navbar")
   }
-
-  function focusDataTable() {
-    dataTable.value.focus()
-  }
-
+  
   function notify({ title, message }) {
     emits("notification", { title, message })
-  }
-
-  function openCreateModal() {
-    openModal({ mode: "create" })
-  }
-
-  async function updateItemAndFetchData(index, client) {
-    const hasNoClient = !client
-    if (hasNoClient) {
-      return
-    }
-
-    const response = await updateClient(client)
-    const { acknowledged, error } = response
-    if (error) {
-      throw error
-    }
-
-    if (!acknowledged) {
-      notify({
-        title: "Unchanged.",
-        message: `<span class="italic font-bold">${client.firstName} ${client.lastName}</span> is the same.`,
-      })
-      return
-    }
-
-    notify({
-      title: "Successfully Saved!",
-      message: `<span class="italic font-bold">${client.firstName} ${client.lastName}</span> was updated!`,
-    })
-
-    data.value[index] = client
-
-    // * No need to ensure flow of events as it is the last action
-    // * Hence we do not use the await here
-    fetchTableData()
-  }
-
-  async function openModal(response) {
-    const { mode, index } = response
-    switch (mode) {
-      case "view": {
-        viewClient(index)
-        break
-      }
-
-      case "edit": {
-        editClientViaModal(index)
-        break
-      }
-
-      case "create": {
-        createNewClientViaModal()
-        break
-      }
-
-      default: {
-        debugger
-        console.log("Open modal mode is not recognised. 🤷‍♂️")
-        break
-      }
-    }
-  }
-
-  async function viewClient(clientIndex) {
-    selectedClient.value = getCopy(data.value[clientIndex])
-    const client = await modal.value.open()
-    focusDataTable()
-
-    const hasNoClient = !client
-    if (hasNoClient) {
-      return
-    }
-
-    await updateItemAndFetchData(clientIndex, client)
-  }
-
-  // * same as `viewClient` client for now; subject to change soon.
-  async function editClientViaModal(clientIndex) {
-    selectedClient.value = getCopy(data.value[clientIndex])
-    const client = await modal.value.open()
-    focusDataTable()
-
-    const hasNoClient = !client
-    if (hasNoClient) {
-      return
-    }
-
-    await updateItemAndFetchData(index, client)
-  }
-
-  async function createNewClientViaModal(previousClient = null) {
-    // * When back-end fails to create the client,
-    // * we re-open the modal with the same data
-    selectedClient.value = previousClient || new Client()
-    const client = await modal.value.open()
-    focusDataTable()
-
-    const hasNoClient = !client
-    if (hasNoClient) {
-      return
-    }
-
-    try {
-      const response = await createClient(client)
-      console.log(response)
-      notify({
-        title: "Successfully created!",
-        message: `<span class="italic font-bold">${client.firstName} ${client.lastName}</span> was created!`,
-      })
-    } catch (error) {
-      await alert({
-        title: "⚠️ OOPS! ⚠️",
-        text: `Failed to create the client. Please try again.`,
-      })
-
-      return createNewClientViaModal(client)
-    }
-
-    await fetchTableData()
   }
 </script>
 
---
+++ front/src/views/dashboard.js
@@ -0,0 +1,254 @@
+import { ref } from "vue"
+
+import useHelpers from "../app/helpers"
+
+import Client from "../models/Client"
+
+import { getClients, createClient, updateClient } from "../api/client"
+
+const { searchStringInList, getCopy, ctrlPlus } = useHelpers()
+
+
+let initialData = []
+
+const data = ref([])
+const selectedClient = ref({})
+
+// * template refs
+const modal = ref(null)
+const toolbar = ref(null)
+const dataTable = ref(null)
+
+
+export default function useDashboard({ notify } = {}) {
+
+  return {
+    // * template refs
+    modal,
+    toolbar,
+    dataTable,
+
+    // *template refs access methods
+    focusDataTable,
+    focusSearch,
+
+    // * data refs
+    data,
+    initialData,
+    selectedClient,
+
+    // * data methods with data & template impacts
+    fetchTableData,
+    filter,
+    filterData,
+    openModal: openModalWrapper,
+    openCreateModal: openCreateModalWrapper,
+    createNewClientViaModal,
+    viewClient: viewClientWrapper,
+    editClientViaModal: editClientViaModalWrapper,
+    updateItemAndFetchData: updateItemAndFetchDataWrapper,
+  }
+
+
+
+  function openModalWrapper(...args) {
+    return openModal(notify, ...args)
+  }
+
+  function openCreateModalWrapper(...args) {
+    return openCreateModal(notify, ...args)
+  }
+
+  function updateItemAndFetchDataWrapper(...args) {
+    return updateItemAndFetchData(notify, ...args)
+  }
+
+  function viewClientWrapper(...args) {
+    return viewClient(notify, ...args)
+  }
+
+  function editClientViaModalWrapper(...args) {
+    return editClientViaModal(notify, ...args)
+  }
+}
+
+function openCreateModal(notify) {
+  openModal(notify, { mode: "create" })
+}
+
+function focusDataTable() {
+  dataTable.value.focus()
+}
+
+function focusSearch() {
+  toolbar.value.focusSearch()
+}
+
+async function fetchTableData() {
+  const response = await getClients()
+  const dataFromServer = await response.json()
+  data.value = [...dataFromServer]
+  initialData = [...dataFromServer]
+}
+
+
+function filter(field, value) {
+  switch (field) {
+    case "is-from-codementor": {
+      if (!value) {
+        return (data.value = initialData)
+      }
+
+      data.value = initialData.filter(item => item.isCodementor === value)
+      break
+    }
+
+    case "tags": {
+      if (!value || !value.length) {
+        return (data.value = initialData)
+      }
+
+      const filteredData = initialData.filter(item =>
+        value.every(tag => !!item.tags.includes(tag))
+      )
+
+      data.value = filteredData || initialData
+      break
+    }
+
+    default: {
+      data.value = initialData
+      break
+    }
+  }
+}
+
+function filterData(searchString) {
+  const noSearchString = !searchString
+  if (noSearchString) {
+    data.value = initialData
+    return
+  }
+
+  const filteredList = searchStringInList(initialData, searchString, {
+    isObejectList: true,
+  })
+  data.value = [...filteredList]
+}
+
+
+async function openModal(notify, response) {
+  const { mode, index } = response
+  switch (mode) {
+    case "view": {
+      viewClient(notify, index)
+      break
+    }
+
+    case "edit": {
+      editClientViaModal(notify, index)
+      break
+    }
+
+    case "create": {
+      createNewClientViaModal(notify)
+      break
+    }
+
+    default: {
+      debugger
+      console.log("Open modal mode is not recognised. 🤷‍♂️")
+      break
+    }
+  }
+}
+
+async function createNewClientViaModal(notify, previousClient = null) {
+  // * When back-end fails to create the client,
+  // * we re-open the modal with the same data
+  selectedClient.value = previousClient || new Client()
+  const client = await modal.value.open()
+  focusDataTable()
+
+  const hasNoClient = !client
+  if (hasNoClient) {
+    return
+  }
+
+  try {
+    const response = await createClient(client)
+    console.log(response)
+    notify({
+      title: "Successfully created!",
+      message: `<span class="italic font-bold">${client.firstName} ${client.lastName}</span> was created!`,
+    })
+  } catch (error) {
+    await alert({
+      title: "⚠️ OOPS! ⚠️",
+      text: `Failed to create the client. Please try again.`,
+    })
+
+    return createNewClientViaModal(notify, client)
+  }
+
+  await fetchTableData()
+}
+
+async function viewClient(notify, clientIndex) {
+  selectedClient.value = getCopy(data.value[clientIndex])
+  const client = await modal.value.open()
+  focusDataTable()
+
+  const hasNoClient = !client
+  if (hasNoClient) {
+    return
+  }
+  await updateItemAndFetchData(notify, clientIndex, client)
+}
+
+// * same as `viewClient` client for now; subject to change soon.
+async function editClientViaModal(notify, clientIndex) {
+  selectedClient.value = getCopy(data.value[clientIndex])
+  const client = await modal.value.open()
+  focusDataTable()
+
+  const hasNoClient = !client
+  if (hasNoClient) {
+    return
+  }
+
+  await updateItemAndFetchData(notify, clientIndex, client)
+}
+
+async function updateItemAndFetchData(notify, index, client) {
+  const hasNoClient = !client
+  if (hasNoClient) {
+    console.warn("Tried to update but had no client")
+    return
+  }
+
+  const response = await updateClient(client)
+  const { acknowledged, error } = response
+  if (error) {
+    throw error
+  }
+
+  if (!acknowledged) {
+    notify({
+      title: "Unchanged.",
+      message: `<span class="italic font-bold">${client.firstName} ${client.lastName}</span> is the same.`,
+    })
+    return
+  }
+
+  notify({
+    title: "Successfully Saved!",
+    message: `<span class="italic font-bold">${client.firstName} ${client.lastName}</span> was updated!`,
+  })
+
+  data.value[index] = client
+
+  // * No need to ensure flow of events as it is the last action
+  // * Hence we do not use the await here
+  fetchTableData()
+}
