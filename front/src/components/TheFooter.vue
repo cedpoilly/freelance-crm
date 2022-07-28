c38deb6b4fe4debb9d011d38ef8b163137206b82
Cedric Poilly <cedric.poilly@theredpandas.com>
2022-07-28 17:15:13 +0400

    Footer update

--- front/src/components/TheFooter.vue
+++ front/src/components/TheFooter.vue
@@ -1,17 +1,29 @@
 <template>
   <section class="footer">
-    <h3 class="footer-text">This is the app footer.</h3>
+    <h3 class="footer-text">
+      <span class="app-name">Freelance CRM - </span>
+      Made with 💙 by
+      <a class="link" href="mailto:cedpoilly@gmail.com">cedpoilly</a>
+    </h3>
   </section>
 </template>
 
 <style lang="scss" scoped>
   .footer {
-    @apply absolute bottom-0;
-    @apply w-full h-12 px-10 py-4 flex;
-    @apply bg-sky-100 text-sky-900 font-bold;
+    @apply absolute grid content-center;
+    @apply w-full h-10 px-4 py-1 bottom-0;
+    @apply bg-blue-100 text-sky-600;
 
     &-text {
-      @apply opacity-50;
+      @apply w-full opacity-70 text-center;
+    }
+
+    .app-name {
+      @apply hidden md:inline-block italic;
+    }
+
+    .link {
+      @apply font-bold;
     }
   }
 </style>
