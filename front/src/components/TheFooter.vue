d1213f2934c03f00f143601dc825645baf221596
Cedric Poilly <cedric.poilly@theredpandas.com>
2022-07-29 23:51:53 +0400

    Footer styling

--- front/src/components/TheFooter.vue
+++ front/src/components/TheFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <section class="footer">
-    <h3 class="footer-text">
+  <section class="footer text-micro">
+    <h3 class="footer-text text-micro">
       <span class="app-name">Freelance CRM - </span>
       Made with 💙 by
       <a class="link" href="mailto:cedpoilly@gmail.com">cedpoilly</a>
@@ -11,8 +11,8 @@
 <style lang="scss" scoped>
   .footer {
     @apply grid content-center;
-    @apply w-full h-10 px-4 py-1 bottom-0;
-    @apply bg-teal-100 dark:bg-teal-900 text-sky-600 dark:text-sky-100;
+    @apply w-full h-8 md:h-10 px-4 md:py-1 bottom-0;
+    @apply bg-teal-100 dark:bg-slate-800 text-sky-600 dark:text-sky-100;
 
     &-text {
       @apply w-full opacity-70 text-center;
